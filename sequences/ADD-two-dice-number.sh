78cdb014175a79c53f2bd98041139218dbd5ef36
amit <[amirana14325@gmail.com]>
2021-02-09 00:47:45 +0530

    Adding -- sequences shell problems

---
+++ sequences/ADD-two-dice-number.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+x=$((RANDOM%10))
+y=$((RANDOM%10))
+a=0
+b=0
+if [ $x -lt 7 -a $x -gt 0 ]
+then 
+	a=$x
+	echo $a
+else
+	echo X is wrong
+fi
+if [ $y -lt 7 -a $y -gt 0 ]
+then
+	b=$y
+	if [ $a != 0 ]
+	then
+	z=$(( $a + $b ))
+	echo z
+	else
+	echo sory first  number is wrong
+	fi
+else
+	echo wrong
+fi
