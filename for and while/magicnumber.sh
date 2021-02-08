5fa83ff2560d0fbbd27023654be78a18d2d1c4b0
amit <[amirana14325@gmail.com]>
2021-02-09 02:10:22 +0530

    magic number  problem

--- for and while/magicnumber.sh
+++ for and while/magicnumber.sh
@@ -10,3 +10,29 @@ do
 		echo ""number is not prime
 	fi
 done
+
+===============================================================
+#!/bin/bash -x
+
+echo "Think number Between 1 to 100"
+
+min=0
+max=100
+
+while [ $min != $max ]
+do
+	Avg=$((($min+$max)/2))
+	read -p "Is your Number is Less Than $Avg if yes write 1 and if greter than then write 2 ..only 1 or 2 " n
+	if [ $n -eq 1 ]
+	then
+		max=$((($min+$max)/2))
+	elif [ $n -eq 2 ]
+	then
+		min=$((($min+$max)/2))
+	else
+		min=$Avg
+		max=$Avg
+	fi
+done
+
+echo "Your Magic Number is: $Avg"
